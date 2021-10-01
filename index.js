4fdc56499255ee1880df4f1bd3a5abcac2fcab0c
Neragin <niranjan@nmathirajan.xyz>
2021-09-30 21:24:23 -0700

    allow prefix to be read from both config file aswell as the environment.

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ require('dotenv').config(); // processes .env key/values
 const {printHello} = require('./commands/hello'); // this imports the file 'hello.js'. be sure to include all of the functions you created in your own file within the '{}' and separated by commas.
 const {printServerID} = require('./commands/server');
 // const {functionToSendToIndexDotJS} = require('./commands/example');
-
-
+const token = process.env.token ?? config.token ?? "No Token was specified." //default to ! if no token is found
+const prefix = process.env.prefix ?? config.prefix ?? "!" //default to ! if no prefix is found
 // ^^^^ keep file imports above this comment 
 
 // Section C - confirms that the bot is online
@@ -24,19 +24,19 @@ client.on('ready', () => {
 client.on('messageCreate', msg => {
 
   // show server id
-  if (msg.content === "!server id") {
+  if (msg.content === `${prefix}server id`) {
     let nameOfGuild = printServerID() + ".name";
     msg.channel.send(msg.guild.name);
     //msg.channel.send(msg.nameOfGuild);
     //console.log(nameOfGuild);
   } 
   // say 'hello'
-  if (msg.content === "!hello") {
+  if (msg.content === `${prefix}hello`) {
     msg.channel.send(printHello());
   } 
 });
 
 // verifies that the bot is logged in with access
-client.login(process.env.token); // .env files allow you to host your bot on heroku.com
+client.login(token); // .env files allow you to host your bot on heroku.com
 // to use the config.json instead use:
 // client.login(config.token);
